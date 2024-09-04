fae0dab2207759534e86d06eda36ba4d0d2316f5
Rafael Santoni <rafasantoni.dev@gmail.com>
2024-09-04 00:28:35 -0300

    Aula #009: Criada classe Method base do projeto

--- public/bootstrap/bootstrap.php
+++ public/bootstrap/bootstrap.php
@@ -1,6 +1,10 @@
 <?php
 
 $controller = new App\Controllers\Controller;
+$object = $controller->controller();
+
+$method = new App\Controllers\Method;
 
 // dump($controller->getController());
 // dump($controller->controller());
+// dump($method->method($object));
