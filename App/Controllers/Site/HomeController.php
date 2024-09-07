9f26c69d289ac04d383dcf3119b3a5efa83b8350
Rafael Santoni <rafasantoni.dev@gmail.com>
2024-09-07 01:08:57 -0300

    Aula #030: Realizado ajustes no layout da página principal e no arquivo de estilos e adicionada as imagens do site

--- App/Controllers/Site/HomeController.php
+++ App/Controllers/Site/HomeController.php
@@ -15,15 +15,15 @@ class HomeController extends BaseController {
 		// dump($user->fetchAll());
 		// dump($user->find('id',3));
 
-		$produtoRepository = new ProdutoRepository;
-		dump($produtoRepository->listarProdutosOrdenadosComLimite(3));
+		// $produtoRepository = new ProdutoRepository;
+		// dump($produtoRepository->listarProdutosOrdenadosComLimite(3));
 
-		// $dados = [
-		// 	'titulo' => 'Loja Virtual - Eletrônicos | SmartPhones | Periféricos Para PC - RS-Dev'
-		// ];
+		$dados = [
+			'titulo' => 'Loja Virtual - Eletrônicos | SmartPhones | Periféricos Para PC - RS-Dev'
+		];
 
-		// $template = $this->twig->loadTemplate('site_home.html');
-		// $template->display($dados);
+		$template = $this->twig->loadTemplate('site_home.html');
+		$template->display($dados);
 		
 	}
 
