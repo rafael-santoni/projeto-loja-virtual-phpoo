9074002b83fe5fe63f4652109854c40f2ad3aea6
Rafael Santoni <rafasantoni.dev@gmail.com>
2024-09-07 13:23:58 -0300

    Aula #034: Adicionada a listagem de produtos em promoção no conteúdo principal da página

--- App/Controllers/Site/HomeController.php
+++ App/Controllers/Site/HomeController.php
@@ -11,11 +11,17 @@ class HomeController extends BaseController {
 	public function index()	{
 
 		$produtoRepository = new ProdutoRepository;
+
+		// Listar pelo destaque
 		$produtosDestaque = $produtoRepository->listarProdutosOrdenadosPeloDestaque(6);
+
+		// Listar pela promoção
+		$produtosPromocao = $produtoRepository->listarProdutosPromocao(6);
 
 		$dados = [
 			'titulo' => 'Loja Virtual - Eletrônicos | SmartPhones | Periféricos Para PC - RS-Dev',
-			'produtos' => $produtosDestaque
+			'produtos' => $produtosDestaque,
+			'produtosPromocao' => $produtosPromocao
 		];
 
 		$template = $this->twig->loadTemplate('site_home.html');
--- App/Repositories/Site/ProdutoRepository.php
+++ App/Repositories/Site/ProdutoRepository.php
@@ -12,9 +12,10 @@ class ProdutoRepository {
 		$this->produto = new ProdutoModel;
 	}
 
+	// Listar o último produto adicionado
 	public function ultimoProdutoAdicionado(){
 
-		$sql = "SELECT produto_nome,produto_slug,produto_foto FROM {$this->produto->table} ORDER BY id DESC";
+		$sql = "SELECT produto_nome,produto_slug,produto_foto,produto_promocao,produto_valor,produto_valor_promocao FROM {$this->produto->table} ORDER BY id DESC";
 		$this->produto->typeDatabase->prepare($sql);
 		$this->produto->typeDatabase->execute();
 
@@ -22,10 +23,21 @@ class ProdutoRepository {
 		
 	}
 
+	// Listar os produtos em destaque
 	public function listarProdutosOrdenadosPeloDestaque($limite){
 
 		$sql = "SELECT * FROM {$this->produto->table} ORDER BY produto_destaque=1 DESC LIMIT {$limite}";
-		// $sql = "SELECT * FROM {$this->produto->table} ORDER BY id DESC LIMIT {$limite}";
+		$this->produto->typeDatabase->prepare($sql);
+		$this->produto->typeDatabase->execute();
+
+		return $this->produto->typeDatabase->fetchAll();
+		
+	}
+
+	// Listar os produtos em promoção
+	public function listarProdutosPromocao($limite){
+
+		$sql = "SELECT * FROM {$this->produto->table} WHERE produto_promocao=1 ORDER BY produto_destaque=1 DESC LIMIT {$limite}";
 		$this->produto->typeDatabase->prepare($sql);
 		$this->produto->typeDatabase->execute();
 
