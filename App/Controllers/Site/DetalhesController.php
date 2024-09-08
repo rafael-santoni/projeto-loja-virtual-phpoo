43bc8e98e32853ed8ee5e51e936a663596bc986d
Rafael Santoni <rafasantoni.dev@gmail.com>
2024-09-07 22:53:17 -0300

    Aula #038: Criada a página View de exibição dos detalhes de um produto

---
+++ App/Controllers/Site/DetalhesController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controllers\Site;
+
+use App\Controllers\BaseController;
+use App\Models\Site\ProdutoModel;
+
+class DetalhesController extends BaseController {
+
+	private $produto;
+
+	public function __construct(){
+		$this->produto = new ProdutoModel;
+	}
+
+	public function index($params){
+
+		$produtoEncontrado = $this->produto->find('produto_slug', $params[0]);
+		
+		$dados = [
+			'titulo' => 'Detalhes do produto '.$produtoEncontrado->produto_nome,
+			'produto' => $produtoEncontrado
+		];
+
+		$template = $this->twig->loadTemplate('site_detalhes.html');
+		$template->display($dados);
+		
+	}
+
+}
