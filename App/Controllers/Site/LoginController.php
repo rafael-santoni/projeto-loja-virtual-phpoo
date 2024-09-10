5682072f9578ae845379e42b344da2c5f603fc69
Rafael Santoni <rafasantoni.dev@gmail.com>
2024-09-10 15:07:13 -0300

    Aula #058: Criada a classe Password para manipular as senhas dentro do site

--- App/Controllers/Site/LoginController.php
+++ App/Controllers/Site/LoginController.php
@@ -3,10 +3,14 @@
 namespace App\Controllers\Site;
 
 use App\Controllers\BaseController;
+use App\Classes\Password;
 
 class LoginController extends BaseController {
 
     public function index(){
+
+        $password = new Password;
+        dump($password->hash('1234'));
 
         $dados = [
             'titulo' => 'Loja Virtual - RS-Dev | Login',
