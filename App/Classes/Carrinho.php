80bfe7281c64412180c5a617c56f183611907ca3
Rafael Santoni <rafasantoni.dev@gmail.com>
2024-09-14 15:02:40 -0300

    Aula #089: Criada a Classe Model para a tabela Carrinho e adicionado o código para adicionar e alterar a quantidade de um produto na tabela carrinho

--- App/Classes/Carrinho.php
+++ App/Classes/Carrinho.php
@@ -4,17 +4,20 @@ namespace App\Classes;
 
 use App\Classes\StatusCarrinho;
 use App\Classes\Estoque;
+use App\Models\Site\CarrinhoModel;
 
 class Carrinho {
 
     private $statusCarrinho;
     private $estoque;
+    private $carrinhoModel;
 
     public function __construct(){
 
         $this->statusCarrinho = new StatusCarrinho;
         $this->statusCarrinho->criarCarrinho();
         $this->estoque = new Estoque;
+        $this->carrinhoModel = new CarrinhoModel;
 
     }
 
@@ -23,11 +26,23 @@ class Carrinho {
         if($this->estoque->estoqueAtual($id) > 1) {
 
             if($this->statusCarrinho->produtoEstaNoCarrinho($id)) {
+
                 $_SESSION['carrinho'][$id] += 1;
+                $this->carrinhoModel->update($id, $this->produtoCarrinho($id));
+
             } else {
+
                 $_SESSION['carrinho'][$id] = 1;
+                $this->carrinhoModel->add([
+                    1 => $id,
+                    2 => 1,
+                    3 => '123',
+                    4 => date('Y-m-d H:i:s'),
+                    5 => date('Y-m-d H:i:s', strtotime('+1minute'))
+                ]);
+
             }
-            
+
         }
 
     }
---
+++ App/Models/Site/CarrinhoModel.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Models\Site;
+
+use App\Models\Model;
+
+class CarrinhoModel extends Model {
+
+    public $table = 'carrinho';
+
+    public function add(Array $attributes){
+
+        $sql = "INSERT INTO {$this->table} (produto,quantidade,sessao,created_at,expire) VALUES (?,?,?,?,?)";
+        $this->typeDatabase->prepare($sql);
+
+        foreach ($attributes as $key=>$value) {
+            $this->typeDatabase->bindValue($key,$value);
+        }
+
+        return $this->typeDatabase->execute();
+
+    }
+
+    public function update($id, $qtd){
+
+        $sql = "UPDATE {this->table} SET quantidade = ? WHERE produto = ?";
+        $this->typeDatabase->prepare();
+        $this->typeDatabase->bindValue(1, $qtd);
+        $this->typeDatabase->bindValue(2, $id);
+        $this->typeDatabase->execute();
+
+        return $this->typeDatabase->rowCount();
+
+    }
+
+    public function remove($id){
+
+        $sql = "DELETE FROM {$this->table} WHERE produto = ?";
+        $this->typeDatabase->prepare($sql);
+        $this->typeDatabase->bindValue(1, $id);
+
+        return $this->typeDatabase->execute();
+
+    }
+
+}
