4f24c198f78d9fca6494c95429f89e241a1eadee
Rafael Santoni <rafasantoni.dev@gmail.com>
2024-09-14 14:09:11 -0300

    Aula #088: Adicionado o código que verifica quantidade de produto no estoque antes de adicionar no carrinho

--- App/Classes/Carrinho.php
+++ App/Classes/Carrinho.php
@@ -3,24 +3,31 @@
 namespace App\Classes;
 
 use App\Classes\StatusCarrinho;
+use App\Classes\Estoque;
 
 class Carrinho {
 
     private $statusCarrinho;
+    private $estoque;
 
     public function __construct(){
 
         $this->statusCarrinho = new StatusCarrinho;
         $this->statusCarrinho->criarCarrinho();
+        $this->estoque = new Estoque;
 
     }
 
     public function add($id){
 
-        if($this->statusCarrinho->produtoEstaNoCarrinho($id)) {
-            $_SESSION['carrinho'][$id] += 1;
-        } else {
-            $_SESSION['carrinho'][$id] = 1;
+        if($this->estoque->estoqueAtual($id) > 1) {
+
+            if($this->statusCarrinho->produtoEstaNoCarrinho($id)) {
+                $_SESSION['carrinho'][$id] += 1;
+            } else {
+                $_SESSION['carrinho'][$id] = 1;
+            }
+            
         }
 
     }
